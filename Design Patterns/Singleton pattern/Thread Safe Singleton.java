ccff390e80a3cb99da918ea7b63533de200a8686
Biplob <66861180+Biplob68@users.noreply.github.com>
2022-01-16 11:52:07 +0600

    Update Thread Safe Singleton.java

--- Design Patterns/Singleton pattern/Thread Safe Singleton.java
+++ Design Patterns/Singleton pattern/Thread Safe Singleton.java
@@ -26,3 +26,18 @@ public class ThreadSafeSingleton {
 		}
 
 }
+
+
+/*  // Double Checked Locking
+	
+public static ThreadSafeSingleton getInstanceUsingDoubleLocking(){
+    if(instance == null){
+        synchronized (ThreadSafeSingleton.class) {
+            if(instance == null){
+                instance = new ThreadSafeSingleton();
+            }
+        }
+    }
+    return instance;
+}
+*/
